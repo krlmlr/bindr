1dfed1b80198eb9e992e157c12372494e3b01082
Kirill Müller <krlmlr+r@mailbox.org>
2016-10-15 16:20:50 +0200

    description and package docs

--- R/bindr-package.R
+++ R/bindr-package.R
@@ -1,5 +1,4 @@
-#' bindr.
-#'
-#' @name bindr
-#' @docType package
-NULL
+#' @details
+#' See [create_env()] for creating an environment populated with active bindings,
+#' and [populate_env()] for populating an existing environment.
+"_PACKAGE"

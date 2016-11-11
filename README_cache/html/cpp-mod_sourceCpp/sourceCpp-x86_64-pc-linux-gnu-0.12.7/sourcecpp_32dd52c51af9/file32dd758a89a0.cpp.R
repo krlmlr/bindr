2b285a5070a056fd7a1fb6c669b200bfd20ba93a
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-11 11:15:47 +0000

    Deploy from Travis build 41 [ci skip]
    Build URL: https://travis-ci.org/krlmlr/bindr/builds/175040026
    Commit: 950c527d3a9fd3660ce61d9a6ddb6650f05f969f

---
+++ README_cache/html/cpp-mod_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-0.12.7/sourcecpp_32dd52c51af9/file32dd758a89a0.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('/home/travis/build/krlmlr/bindr/docs/README_cache/html/cpp-mod_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-0.12.7/sourcecpp_32dd52c51af9/sourceCpp_2.so')
+
+change_case <- Rcpp:::sourceCppFunction(function(name, to_upper = FALSE) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_change_case')
+
+rm(`.sourceCpp_1_DLLInfo`)
